5be4060f078f49ac8ec6c22f02bbb7e1be7c4252
Maryanne Wachter <maryanne.wachter@gmail.com>
2023-01-07 15:16:55 -0800

    Working Map React component with local tile server

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from "react";
 import Map from "./Map";
-import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div>
-      <Helmet>
-        <script
-          src="https://unpkg.com/protomaps@1.22.0/dist/protomaps.min.js"
-          type="text/javascript"
-        />
-      </Helmet>
       <Map />
     </div>
   );
--- src/Map.jsx
+++ src/Map.jsx
@@ -7,41 +7,22 @@ const Map = () => {
     width: "100%",
     height: "100vh",
   };
-
-  const MAP_TILE = L.tileLayer(
-    `https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`,
-    {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }
-  );
-  const mapParams = {
-    center: [37.0902, -95.7129],
-    zoom: 3,
-    zoomControl: false,
-    maxBounds: L.latLngBounds(L.latLng(-150, -240), L.latLng(150, 240)),
-    layers: [MAP_TILE],
-  };
-
   useEffect(() => {
-    const container = L.DomUtil.get("map2");
-
+    const container = L.DomUtil.get("pmMap");
+    const map = L.map("pmMap");
     if (container != null) {
       container._leaflet_id = null;
     }
-
-    // const map = L.map("map2");
-    // const layer = window.protomaps.leafletLayer({
-    //   url: "http://localhost:8080/Seattle_WA.pmtiles",
-    // });
-    // console.log(layer);
-    // layer.addTo(map);
-    const map = L.map("map2", mapParams);
+    map.setView(new L.LatLng(47.608, -122.335), 16);
+    var layer = window.protomaps.leafletLayer({
+      url: "http://localhost:8080/Seattle_WA.pmtiles",
+    });
+    layer.addTo(map);
   }, []);
 
   return (
     <div>
-      <div id="map2" style={mapStyles} />
+      <div id="pmMap" style={mapStyles} />
     </div>
   );
 };
